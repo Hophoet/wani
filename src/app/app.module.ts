b1a552feaf4d53b25bb525f14764d30ddba91760
hophoet <hophoet@gmail.com>
2021-04-20 11:33:42 +0000

    (module) - set the created component to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TestComponent } from './test/test.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Section1Component } from './section1/section1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    NavbarComponent,
+    Section1Component
   ],
   imports: [
     BrowserModule,
