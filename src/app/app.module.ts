5779490058949a67280b6d1dbe237de4d8d6a445
hophoet <hophoet@gmail.com>
2021-04-21 16:53:03 +0000

    (module) - set the created components section1, section2 and section2 to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TestComponent } from './test/test.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
+import { Section3Component } from './section3/section3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     NavbarComponent,
-    Section1Component
+    Section1Component,
+    Section2Component,
+    Section3Component
   ],
   imports: [
     BrowserModule,
